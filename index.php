6313fe6fd280c35badeb89a7261e28f838b04743
makmugaming <helmiar527@gmail.com>
2022-03-07 09:37:17 +0700

    Membuat Home

--- index.php
+++ index.php
@@ -1,40 +1,55 @@
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="shortcut icon" href="#">
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
-    <title></title>
-  </head>
-  <body>
-    <!-- NavBar -->
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <div class="container">
-        <a class="navbar-brand" href="#">Navbar</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-          <ul class="navbar-nav">
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">Home</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Features</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Pricing</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link disabled">Disabled</a>
-            </li>
-          </ul>
-        </div>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="shortcut icon" href="#">
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+  <!-- My CSS -->
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+  <title></title>
+</head>
+<body>
+  <!-- NavBar -->
+  <nav class="navbar navbar-expand-lg navbar-dark bg-warning shadow-sm">
+    <div class="container">
+      <a class="navbar-brand" href="index.php">HelmiAR527</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ms-auto">
+          <li class="nav-item">
+            <a class="nav-link active btn btn-" aria-current="page" href="index.php">Beranda</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Tentang</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Kontak</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Daftar</a>
+          </li>
+        </ul>
       </div>
-    </nav>
-    <!-- End NavBar -->
-    <!-- Bootstrap JS -->
-    <script type="text/javascript" herf="js/bootstrap.bundle.min.js"></script>
-  </body>
+    </div>
+  </nav>
+  <!-- End NavBar -->
+
+  <!-- Jumbotron -->
+  <section class="jumbotron">
+    <h1 class="display-4 fw-bold">HelmiAR527</h1>
+    <p class="lead">Selamat datang di website pribadi HelmiAR527, dimana di sini saya menyediakan beberapa aplikasi yang mungkin kalian bisa gunakan.</p>
+    <hr class="my-4">
+    <p>Silahkan untuk daftar di bawah untuk login.</p>
+    <a class="btn btn-primary btn-lg" href="#" role="button">Daftar</a>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+      <path fill="#ffffff" fill-opacity="1" d="M0,256L34.3,245.3C68.6,235,137,213,206,213.3C274.3,213,343,235,411,250.7C480,267,549,277,617,250.7C685.7,224,754,160,823,138.7C891.4,117,960,139,1029,144C1097.1,149,1166,139,1234,160C1302.9,181,1371,235,1406,261.3L1440,288L1440,320L1405.7,320C1371.4,320,1303,320,1234,320C1165.7,320,1097,320,1029,320C960,320,891,320,823,320C754.3,320,686,320,617,320C548.6,320,480,320,411,320C342.9,320,274,320,206,320C137.1,320,69,320,34,320L0,320Z"></path>
+    </svg>
+  </section>
+  <!-- Akhir Jumbotron -->
+  <!-- Bootstrap JS -->
+  <script type="text/javascript" herf="js/bootstrap.bundle.min.js"></script>
+</body>
 </html>
